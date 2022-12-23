758dae1182a6a78ff4a48b79f06be98f779d03e1
Gavin Carew <gjcarew@gmail.com>
2022-12-23 10:36:47 -0800

    Passenger class

---
+++ lib/passenger.rb
@@ -0,0 +1,21 @@
+class Passenger
+  attr_reader :name, :age
+
+  def initialize(input_hash)
+    @name = input_hash['name']
+    @age = input_hash['age']
+    @driver  = false
+  end
+
+  def adult?
+    age >= 18
+  end
+
+  def driver?
+    @driver
+  end
+
+  def drive
+    @driver = true
+  end
+end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -5,26 +5,26 @@ RSpec.describe Passenger do
     @passenger = Passenger.new({"name" => "Charlie", "age" => 18})
   end
   it 'exists' do 
-    expect(@passenger).to be_a Passenger
+    expect(@passenger).to be_a(Passenger)
   end
 
-  xit 'has attributes' do 
+  it 'has attributes' do 
     expect(@passenger.name).to eq('Charlie')
     expect(@passenger.age).to eq(18)
   end
 
-  xit 'has a method to tell if a passenger is an adult' do 
+  it 'has a method to tell if a passenger is an adult' do 
     expect(@passenger.adult?).to eq(true)
     passenger2 = Passenger.new({"name" => "Taylor", "age" => 12})
     expect(passenger2.adult?).to eq(false)
   end
 
-  xit 'A passenger is not instantiated as a driver' do
+  it 'A passenger is not instantiated as a driver' do
     expect(@passenger.driver?).to eq(false)
   end
 
-  xit 'A passenger can be made a driver' do 
+  it 'A passenger can be made a driver' do 
     @passenger.drive
-    expect(@passenger.driver?).to eq(false)
+    expect(@passenger.driver?).to eq(true)
   end
 end
